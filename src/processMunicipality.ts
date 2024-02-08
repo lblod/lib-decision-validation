f26084d6e87dc2a2af3f6c6c3b27f2dd074fef6a
Lenny  Bontenakel <leaflock@fangorn.local>
2024-02-08 15:37:52 +0100

    fixed document validation

--- src/processMunicipality.ts
+++ src/processMunicipality.ts
@@ -8,7 +8,6 @@ import { RDFShape, RDFProperty } from './types';
   - publication: object to be validated
   returns:
   - one of the following valuesL: [besluitenlijst, notulen, agenda]
-
 */
 export function determineDocumentType(bindings: Bindings[]): string {
   // Look for document type predicate if it is present
@@ -31,11 +30,9 @@ export function determineDocumentType(bindings: Bindings[]): string {
   return "unknown document type"
 }
 
-export function validateProperty(property: Bindings[]): any {
-  // 
-  console.log(`proper ei ${property}`)
+export function validateProperty(subject: Bindings[], propertyShape: Bindings[]): any {
   let result: any = {}
-  property.forEach(p => {
+  propertyShape.forEach(p => {
     switch(p.get('p')!.value) {
         case "http://www.w3.org/ns/shacl#name": {
           result.name = p.get('o')!.value;
@@ -66,45 +63,63 @@ export function validateProperty(property: Bindings[]): any {
         }
       }
   });
+  result.actualValue = subject
+    .filter(s => s.get('p')!.value === result.path)
+    .map(s => s.get('o')!.value)
+  result.actualCount = result.actualValue.length
+  result.valid = (result.minCount == undefined || result.actualCount >= result.minCount) && 
+    (result.maxCount == undefined || result.actualCount <= result.maxCount);
   return result;
 }
+
 
 /* function to validate a publication 
   param:
   - publication: object to be validated
   returns:
   - contains a report of all missing requirements for a publication
-
 */
 export async function validatePublication(publication: Bindings[], blueprint: Bindings[]) {
-  // check if the publication has all the necessary annotations
-  // it should possess all the right shapes and properties
-  // we start by collecting every shape 
-  const shapes: Bindings[] = blueprint
-    .filter(b => 
-      b.get("p")!.value === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" &&
-        b.get("o")!.value === "http://www.w3.org/ns/shacl#NodeShape")
 
-  const result: RDFShape[] = [];
+  // get the URI of all unique subjects and place them in an array
+  const subjectKeys: string[] = [ ...new Set(publication.map((p) => p.get('s')!.value)) ]
+  
+  const result: any[] = []
   // we should save the counts of properties per subject
-  shapes.forEach(shape => {
-    switch(shape.get('p')!.value) {
-      case "http://www.w3.org/ns/shacl#property": {
-        const newProperty = blueprint
-          .filter(b => b.get('s')!.value === shape.get('o')!.value)
-        const validatedProperty = validateProperty(newProperty)
-        console.log(`validated property ${JSON.stringify(validatedProperty)}`)
-        break;
+  subjectKeys.forEach(subjectKey => {
+    const subject: Bindings[] = publication.filter(p => p.get('s')!.value === subjectKey);
+    // console.log(`subject: ${subject}`)  
+    
+    const subjectType: string | undefined = subject
+    .find(s => s.get('p')!.value === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+    )?.get('o')!.value;
+    const blueprintShapeKey: string | undefined = blueprint
+    .find(b => b.get('p')!.value === "http://www.w3.org/ns/shacl#targetClass" &&
+    b.get('o')!.value === subjectType
+    )?.get('s')!.value
+    const blueprintShape: Bindings[] = blueprint
+    .filter(b => b.get('s')!.value === blueprintShapeKey
+    )
+    const propertyKeys: string[] = blueprintShape
+    .filter((b) => b.get("p")!.value === "http://www.w3.org/ns/shacl#property") 
+    .map(b => b.get('o')!.value);
+    
+    if(subject.length > 0 && blueprintShape.length > 0) {
+      let resultSubject: any = {
+        url: subjectKey,
+        type: subjectType,
+        usedShape: blueprintShapeKey,
+        validatedProperties: []
       }
-      case "http://www.w3.org/ns/shacl#targetClass": {
-        break;
-      }
-      default: {
-        console.log(`default ${shape.get('p')!.value}`)
-      }
+      propertyKeys.forEach(propertyKey => {
+        const propertyShape: Bindings[] = blueprint.filter(b => b.get('s')!.value === propertyKey)
+        resultSubject.validatedProperties.push(validateProperty(subject, propertyShape))
+        // console.log(`property: ${propertyShape}`)
+      });
+      result.push(resultSubject);
     }
   });
   // and reflect whether or not it exceeds or subceeds the required counts
   // This in turn will be saved in a 'status' attribute
-
+  return result
 }
--- src/tests/processMunicipality.test.ts
+++ src/tests/processMunicipality.test.ts
@@ -1,5 +1,5 @@
-
 import * as fs from 'fs';
+
 import { Bindings } from '@comunica/types';
 import { determineDocumentType, validatePublication } from '../processMunicipality';
 import { fetchDocument, getBlueprintOfDocumentType, getPublicationFromFileContent } from '../queries'; 
@@ -7,13 +7,12 @@ import { fetchDocument, getBlueprintOfDocumentType, getPublicationFromFileConten
 import { AGENDA_LINK, DOC_LINK, NOTULEN_LINK, TESTHTMLSTRING, TESTSTRING2 } from './testData';
 const PROXY = "https://proxy.linkeddatafragments.org/";
 
-
-
+import { testResult } from './result-ex';
+import { testBlueprint } from './blueprint-ex';
 
 describe('As a vendor, I want the tool to automatically determine the type of the document (agenda, besluitenlijst, notulen)', () => {
   test("determine the type of a document using a link to fetch the publication", async () => {
     const expected: string = "Besluitenlijst";
-
     const document: Bindings[] = await fetchDocument(
       DOC_LINK,
       PROXY
@@ -21,13 +20,13 @@ describe('As a vendor, I want the tool to automatically determine the type of th
     const actual: string = await determineDocumentType(document);
     console.log(`VALUE ${document}`);
     fs.writeFileSync("decision-list-ex.json", `${document}` )
+
     expect(actual).toBe(expected);
   });
 
 
   test("determine the type of a document using a link to fetch the publication", async () => {
     const expected: string = "Agenda";
-
     const document: Bindings[] = await fetchDocument(AGENDA_LINK, PROXY);
     const actual: string = await determineDocumentType(document);
 
@@ -37,38 +36,36 @@ describe('As a vendor, I want the tool to automatically determine the type of th
 
   test("determine the type of a document using a string to fetch the document", async () => {
     const expected: string= "Besluitenlijst";
-
     const document = await getPublicationFromFileContent(TESTHTMLSTRING);
     const actual: string= await determineDocumentType(document)
+
     expect(actual).toBe(expected);
   });
 
   test("detect when the type of a document is unknown", async () => {
     const expected: string = "unknown document type";
-
     const document = await getPublicationFromFileContent(TESTSTRING2);
     const actual: string = await determineDocumentType(document);
+
     expect(actual).toBe(expected);
   });
   
-  test("Get the blueprint for the corresponding document type", async () => {
-      const expected: string = "blueprint for document type";
-
+  // TODO: fix mock data
+  test.skip("Get the blueprint for the corresponding document type", async () => {
+      const expected: any[] = testBlueprint;
       const documentType: string = "BesluitenLijst"
-      const actual = await getBlueprintOfDocumentType(documentType);
-      // console.log(`VALUE ${actual}`);
+      const actual = `${await getBlueprintOfDocumentType(documentType)}`;
+      fs.writeFileSync("blueprint-ex.json", `${actual}`)
       expect(actual).toBe(expected);
   })
 
-  test.only("Validate the publication", async () => {
-    const expected: string = "blueprint for document type";
-
+  // TODO: fix any types
+  test("Validate the publication", async () => {
+    const expected: any[] = testResult
     const blueprint: Bindings[]= await getBlueprintOfDocumentType("BesluitenLijst");
     const publication: Bindings[]= await fetchDocument(DOC_LINK, PROXY);
-    const actual = await validatePublication(publication, blueprint);
-    // console.log(`VALUE ${actual}`);
-    expect(actual).toBe(expected);
+    const actual: any[] = await validatePublication(publication, blueprint);
+    
+    expect(actual).toStrictEqual(expected);
   });
-
-  
 });
