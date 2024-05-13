bf15e583fd7fc42fb9a5b8a33263b94ca332b788
Lenny  Bontenakel <lennyb.0908@gmail.com>
2024-05-13 13:52:32 +0200

    update notulen-link

--- src/queries.ts
+++ src/queries.ts
@@ -50,7 +50,7 @@ export async function fetchDocument(publicationLink: string, proxy: string = def
 
 export async function getBlueprintOfDocumentType(documentType: string): Promise<Bindings[]> {
   const blueprintLink = {
-    Notulen: 'https://raw.githubusercontent.com/lblod/validation-monitoring-module/fix/tests/files/notulen.ttl',
+    Notulen: 'https://raw.githubusercontent.com/lblod/app-validation-tool/enhance/process-unvalidated-properties/files/notulen.ttl',
     Besluitenlijst:
       'https://raw.githubusercontent.com/lblod/poc-decision-source-harvester/master/shapes/decision-list.ttl',
     Agenda: 'https://raw.githubusercontent.com/lblod/poc-decision-source-harvester/master/shapes/basic-agenda.ttl',
